636b608c2faca08feccff9e0ed46f5ba28114743
AtalaySezen <atalaysezen@yahoo.com.tr>
2023-03-02 17:01:15 +0300

    git init

---
+++ assets/scripts/app.js
@@ -0,0 +1,12 @@
+let myLoader = document.getElementById("loader");
+
+document.addEventListener('DOMContentLoaded', () => {
+    myLoader.style.display = "none";
+}
+);
+
+
+console.log(
+    "%c Nothing Here :)",
+    "background: #f25f4c; color: white; "
+)
